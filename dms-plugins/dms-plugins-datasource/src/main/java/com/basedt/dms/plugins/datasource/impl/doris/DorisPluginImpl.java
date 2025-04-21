9d0068dce15138107928d806e4e7bd2d5c4a8715
basedt <basedt@sina.com>
2025-04-21 17:16:48 +0800

    feat: more database supported

--- dms-plugins/dms-plugins-datasource/src/main/java/com/basedt/dms/plugins/datasource/impl/doris/DorisPluginImpl.java
+++ dms-plugins/dms-plugins-datasource/src/main/java/com/basedt/dms/plugins/datasource/impl/doris/DorisPluginImpl.java
@@ -42,19 +42,19 @@ import java.util.stream.Collectors;
 import static com.basedt.dms.plugins.datasource.enums.DbObjectType.*;
 
 @AutoService(DataSourcePlugin.class)
-public class DorisDataSourcePluginImpl extends MysqlPluginImpl {
+public class DorisPluginImpl extends MysqlPluginImpl {
 
-    public DorisDataSourcePluginImpl() {
+    public DorisPluginImpl() {
         super();
         init();
     }
 
-    public DorisDataSourcePluginImpl(Properties properties) {
+    public DorisPluginImpl(Properties properties) {
         super(properties);
         init();
     }
 
-    public DorisDataSourcePluginImpl(String hostName, Integer port, String databaseName, String userName, String password, Map<String, String> attributes) {
+    public DorisPluginImpl(String hostName, Integer port, String databaseName, String userName, String password, Map<String, String> attributes) {
         super(hostName, port, databaseName, userName, password, attributes);
         init();
     }

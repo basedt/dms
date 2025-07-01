64e9ba9e749d0ef4431c078c38270cf42724b215
basedt <basedt@sina.com>
2025-07-02 05:11:03 +0800

    feat: update doris docker file

--- dms-plugins/dms-plugins-datasource/src/test/resources/sql/doris.sql
+++ dms-plugins/dms-plugins-datasource/src/test/resources/sql/doris.sql
@@ -12,3 +12,19 @@ DISTRIBUTED BY HASH (id) BUCKETS 32
     PROPERTIES (
     "replication_num" = "1"
 );
+
+create view v_test as
+select * from table_hash;
+
+select * from v_test;
+
+insert into table_hash(id,name,age) values(1,'tom',10);
+
+create materialized view mv_test
+refresh complete on schedule every 10 hour
+distributed by hash (id) buckets 32
+properties (
+    "replication_num" = "1"
+)
+as
+select * from table_hash;
